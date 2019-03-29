8defbc458e72e709cca2b0cb05f921fdbd62440f
Luis Pérez <luis.x.perez@macys.com>
2019-03-29 11:00:06 -0300

    Configurando Sequelize

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const ConfigurationModule = require('./config/ConfigurationModule');
 const app = express();
 app.use('/', require('./server/src/router/api'))
 
-app.listen(ConfigurationModule.PORT, ConfigurationModule.HOST);
-console.log(`App rodando em http://${ConfigurationModule.HOST}:${ConfigurationModule.PORT}`);
+app.listen(ConfigurationModule.server.PORT, ConfigurationModule.server.HOST);
+console.log(`App rodando em http://${ConfigurationModule.server.HOST}:${ConfigurationModule.server.PORT}`);
